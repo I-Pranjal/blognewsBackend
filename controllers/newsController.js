1739d9ebec496039d398ae7c73a73bdd09be99b7
Pranjal <prathammmishra947@gmail.com>
2025-02-23 16:37:18 +0530

    node-fetch

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,3 +1,5 @@
+import fetch from "node-fetch";
+
 export const latestpost = async (req, res) => {
     try{
         const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=858043bd50e64b938b421764116abca7');
@@ -43,7 +45,7 @@ export const hometechnologynews = async (req, res) => {
     try{
         const response = await fetch('https://newsapi.org/v2/top-headlines?category=technology&country=us&apiKey=331e844264f040ebb02be43ba4a9ddd7');
         const data = await response.json();
-        res.status(200).json(data);
+        res.json(data);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
