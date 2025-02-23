ae5ae699800aa2df0a31d37885a6114f3096944d
Pranjal <prathammmishra947@gmail.com>
2025-02-23 15:05:37 +0530

    updated all news api

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -8,3 +8,57 @@ export const latestpost = async (req, res) => {
         res.status(500).json({message: error.message});
     }
 }
+
+export const homeallnews = async (req, res) => {
+    try{
+        const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=858043bd50e64b938b421764116abca7');
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+export const homesportsnews = async (req, res) => {
+    try{
+        const response = await fetch('https://newsapi.org/v2/top-headlines?category=sports&country=us&apiKey=331e844264f040ebb02be43ba4a9ddd7');
+        const data = await response.json();
+        const titles = data.articles.map(article => article.title);
+        res.status(200).json(titles);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+export const homeentertainmentnews = async (req, res) => {
+    try{
+        const response = await fetch('https://newsapi.org/v2/top-headlines?category=entertainment&country=us&apiKey=331e844264f040ebb02be43ba4a9ddd7');
+        const data = await response.json();
+        const titles = data.articles.map(article => article.title);
+        res.status(200).json(titles);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+export const hometechnologynews = async (req, res) => {
+    try{
+        const response = await fetch('https://newsapi.org/v2/top-headlines?category=technology&country=us&apiKey=331e844264f040ebb02be43ba4a9ddd7');
+        const data = await response.json();
+        const titles = data.articles.map(article => article.title);
+        res.status(200).json(titles);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+export const homebusinessnews = async (req, res) => {
+    try{
+        const response = await fetch('https://newsapi.org/v2/top-headlines?category=business&country=us&apiKey=331e844264f040ebb02be43ba4a9ddd7');
+        const data = await response.json();
+        const titles = data.articles.map(article => article.title);
+        res.status(200).json(titles);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -1,8 +1,14 @@
-import {latestpost} from '../controllers/newsController.js';
+import {latestpost, homeallnews, homebusinessnews, homeentertainmentnews, homesportsnews, hometechnologynews} from '../controllers/newsController.js';
 import express from 'express';
 
 const router = express.Router();
 
 router.get('/latestpost', latestpost);
+router.get('/homeallnews', homeallnews);
+router.get('/homebusinessnews', homebusinessnews);
+router.get('/homeentertainmentnews', homeentertainmentnews);
+router.get('/homesportsnews', homesportsnews);
+router.get('/hometechnologynews', hometechnologynews);
+
 
 export default router;
