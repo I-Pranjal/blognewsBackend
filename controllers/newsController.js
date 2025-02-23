9c373c20f23f97115daa43ad69a3ee06f31ca2fb
Pranjal <prathammmishra947@gmail.com>
2025-02-23 16:13:06 +0530

    changed newController.js

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -23,8 +23,7 @@ export const homesportsnews = async (req, res) => {
     try{
         const response = await fetch('https://newsapi.org/v2/top-headlines?category=sports&country=us&apiKey=331e844264f040ebb02be43ba4a9ddd7');
         const data = await response.json();
-        const titles = data.articles.map(article => article.title);
-        res.status(200).json(titles);
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
@@ -34,8 +33,7 @@ export const homeentertainmentnews = async (req, res) => {
     try{
         const response = await fetch('https://newsapi.org/v2/top-headlines?category=entertainment&country=us&apiKey=331e844264f040ebb02be43ba4a9ddd7');
         const data = await response.json();
-        const titles = data.articles.map(article => article.title);
-        res.status(200).json(titles);
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
@@ -45,8 +43,7 @@ export const hometechnologynews = async (req, res) => {
     try{
         const response = await fetch('https://newsapi.org/v2/top-headlines?category=technology&country=us&apiKey=331e844264f040ebb02be43ba4a9ddd7');
         const data = await response.json();
-        const titles = data.articles.map(article => article.title);
-        res.status(200).json(titles);
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
@@ -56,8 +53,7 @@ export const homebusinessnews = async (req, res) => {
     try{
         const response = await fetch('https://newsapi.org/v2/top-headlines?category=business&country=us&apiKey=331e844264f040ebb02be43ba4a9ddd7');
         const data = await response.json();
-        const titles = data.articles.map(article => article.title);
-        res.status(200).json(titles);
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
